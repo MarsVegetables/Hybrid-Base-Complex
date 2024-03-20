cb426c455774a7a58b87fe361846083d1fe3fcab
MarsVegetables <adanos0@hotmail.com>
2024-03-19 22:14:56 -0500

    update few comments.

--- hybrid_base_complex/hybrid_base_complex.py
+++ hybrid_base_complex/hybrid_base_complex.py
@@ -1,1 +1,2 @@
+# modification is direction made in BaseComplex class
 from base_complex.base_complex import BaseComplex
--- hybrid_base_complex/hybrid_singularity_structure.py
+++ hybrid_base_complex/hybrid_singularity_structure.py
@@ -3,13 +3,10 @@ import random
 
 from base_complex.singularity_structure import SingularityStructure
 
-# build a component mesh based on the hybrid mesh
-# can a component mesh equal to the hybrid mesh?
-
 
 # new version
 class HybridSingularityGraph(SingularityStructure):
     def __init__(self, mesh, iteration_num=0):
         SingularityStructure.__init__(self, mesh, iteration_num)
 
-    # hybrid singularity graph should be about to support component elements
+    # hybrid singularity graph should be able to support component elements
